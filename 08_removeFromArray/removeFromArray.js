9dcadeaa0348bc69d7caf270620a5bd2887a6ec4
Matt <218104632+mabstract25@users.noreply.github.com>
2025-11-06 10:36:45 +0000

    Complete JS exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...cut) {
+    let newarray = [];
+
+    array.forEach((item) => {
+        if(!cut.includes(item)) {
+            newarray.push(item);
+        }
+    })
+
+    return newarray;
 };
 
 // Do not edit below this line
