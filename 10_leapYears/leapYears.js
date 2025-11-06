e9b895c56ad975d136873ee55ef1112e030321d0
Matt <218104632+mabstract25@users.noreply.github.com>
2025-11-06 11:35:21 +0000

    Complete JS exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,28 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+
+    if (
+        isYearDivisibleByFour &&
+        (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+    // This didn't work because of the !== operator.
+
+    // if((year % 4 === 0) && (year % 100 !== 0 && year % 400 === 0)){
+    //     return true;
+    // } else{
+    //     return false;
+    // };
+
+    // if year is divisible by four = leapyear
+    // if year is NOT divisible by 100 && if year IS divisible by 400 = leapyear
 
 };
 
