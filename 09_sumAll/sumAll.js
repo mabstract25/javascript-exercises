c4007d380c24ff2f0dbc2ea658239ae80ac9b499
Matt <218104632+mabstract25@users.noreply.github.com>
2025-11-06 11:06:41 +0000

    Complete JS exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let total = 0;
+
+    for (let i = min; i <= max; i++) {
+        total += i;
+    };
+
+    return total;
 
 };
 
